bc2415b8237cebe939284981f3fb55ec8b09c67c
Felipe Alvim <128483571+fpalvim@users.noreply.github.com>
2024-06-05 10:15:02 +0000

    Pending changes exported from your codespace

--- .learn/resets/01-alert-on-click/index.js
+++ .learn/resets/01-alert-on-click/index.js
@@ -1,7 +1,3 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
-
-document.myFunction = function myFunction() {
-	alert("Ok, it is working.");
-}
--- .learn/resets/03-sum-values/index.js
+++ .learn/resets/03-sum-values/index.js
@@ -1,13 +1,9 @@
-
 // Adding the function to the window makes it globally available
-document.querySelector("#calculate").addEventListener("click", function (){
-
+window.calculateSumListener = function() {
 	// Return the value of the input #firstNumber
 	let stringA = document.getElementById("firstNumber").value;
 	// Return the value of the input #secondNumber
 	let stringB = document.getElementById("secondNumber").value;
 	// Your code here
 	
-	document.getElementById("resultNumber").value = parseInt(stringA) + parseInt(stringB);
-
-})
+};
